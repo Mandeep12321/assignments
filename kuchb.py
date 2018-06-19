fc37477c927c24899aa8d64a3e5d84a63edfe30c
mandeep12321 <amandhiman839@gmail.com>
2018-06-19 16:14:04 +0530

    assignment 4 is submited

---
+++ kuchb.py
@@ -0,0 +1,20 @@
+
+
+
+a=input("enter name of student :")
+aa=int(input("enter marks :"))
+b=input("enter name of student :")
+ab=int(input("enter marks :"))
+c=input("enter name of student :")
+ac=int(input("enter marks :"))
+d={a:aa,b:ab,c:ac}
+
+print(d)
+
+
+#Q2
+
+value_list=list(d.values())
+value_list.sort()
+print(value_list)
+
